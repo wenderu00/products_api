bfd1057c0611273b0708ddf21eb513d9fa9c71f1
wenderu00 <mwmcjr@gmail.com>
2023-11-19 13:38:34 -0300

    swicth product properties tests

--- tests/product/properties.test.js
+++ tests/product/properties.test.js
@@ -1,5 +1,3 @@
-const chai = require('chai');
-const expect = chai.expect;
 const Product = require('../../src/product');
 const userId = 1;
 const id = 1;
@@ -10,42 +8,42 @@ const genericProduct = new Product(userId,id,category, name);
 describe('generic product object properties tests',function(){
     it('generic product object has a category property', function(done){
         expect(genericProduct)
-            .to.have.property("category")
+            .toHaveProperty("category")
         done()
     })
     it('generic product object has a id property', function(done){
         expect(genericProduct)
-            .to.have.property("id")
+            .toHaveProperty("id")
         done()
     })
     it('generic product object has a userId property', function(done){
         expect(genericProduct)
-            .to.have.property("userId");
+            .toHaveProperty("userId");
         done()
     })
     it('generic product object has a name property', function(done){
         expect(genericProduct)
-            .to.have.property("name");
+            .toHaveProperty("name");
         done();
     })
     it('generic product has a name', function(done){
         expect(genericProduct.name)
-            .to.equal(name);
+            .toEqual(name);
         done()
     })
     it('generic product has a category', function(done){
         expect(genericProduct.category)
-            .to.equal(category);
+            .toEqual(category);
         done()
     })
     it('generic product has a id', function(done){
         expect(genericProduct.id)
-            .to.equal(id);
+            .toEqual(id);
         done()
     })
     it('generic product has a userId', function(done){
         expect(genericProduct.userId)
-            .to.equal(userId);
+            .toEqual(userId);
         done()
     })
 })
