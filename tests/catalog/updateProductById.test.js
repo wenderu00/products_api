afd0e3995cbf4ba8dfd8e048437ab345e1c7ea4e
wenderu00 <mwmcjr@gmail.com>
2023-11-19 14:43:11 -0300

    switch catalog updateProductById tests

--- tests/catalog/updateProductById.test.js
+++ tests/catalog/updateProductById.test.js
@@ -1,6 +1,4 @@
-const chai = require('chai');
-const Catalog = require('../../src/catalog');
-const expect = chai.expect;
+const Catalog = require('../../src/catalog.js');
 const catalog = new Catalog();
 const name = "monkey";
 const category = "NFT";
@@ -16,21 +14,21 @@ describe('updateProductById catalog class method tests', function(){
         let invalidId = 50;
         let methodResponse = catalog.updateProductById(invalidId,userId, validObject);
         expect(methodResponse)
-            .to.be.equal(false);
+            .toEqual(false);
         done()
     })
     it('given userId is diferente of product userId then return false', function(done){
         let invalidUserId = 50;
         let methodResponse = catalog.updateProductById(productId, invalidUserId,validObject);
         expect(methodResponse)
-            .to.be.equal(false);
+            .toEqual(false);
         done()
     })
     it('given a object without name and product then return false', function(done){
         let emptyObject = {}
         let methodResponse = catalog.updateProductById(productId, userId, emptyObject);
         expect(methodResponse)
-            .to.be.equal(false)
+            .toEqual(false)
         done()
     })
     it('given a object with name only then returns true', function(done){
@@ -39,21 +37,21 @@ describe('updateProductById catalog class method tests', function(){
         }
         let methodResponse = catalog.updateProductById(productId,userId,objWithName);
         expect(methodResponse)
-            .to.be.equal(true)
+            .toEqual(true)
         done()
     })
     it('after only name to changed has product object name is updated', function(done){
         let productChanged = catalog.getProductById(productId);
         let nameChanged = productChanged.name
         expect(nameChanged)
-            .to.be.equal('mouse')
+            .toEqual('mouse')
         done()
     })
     it('after only name to changed has product object category still unchanged', function(done){
         let productChanged = catalog.getProductById(productId);
         let unchangedCategory = productChanged.category;
         expect(unchangedCategory)
-            .to.be.equal(category)
+            .toEqual(category)
         done()
     })
     it('given a object with category only then update product category', function(done){
@@ -62,21 +60,21 @@ describe('updateProductById catalog class method tests', function(){
         }
         let methodResponse = catalog.updateProductById(productId,userId, objWithCategory)
         expect(methodResponse)
-            .to.be.equal(true)
+            .toEqual(true)
         done()
     })
     it('after only category changed has product object category name is updated', function(done){
         let productChanged = catalog.getProductById(productId);
         let categoryChanged = productChanged.category
         expect(categoryChanged)
-            .to.be.equal("CRIPTO")
+            .toEqual("CRIPTO")
         done()
     })
     it('after only category changed has product object name still unchanged', function(done){
         let productChanged = catalog.getProductById(productId)
         let unchangedName = productChanged.name
         expect(unchangedName)
-            .to.be.equal('mouse');
+            .toEqual('mouse');
         done()
     })
     it('given a object with category and name then update name and category product', function(done){
@@ -86,21 +84,21 @@ describe('updateProductById catalog class method tests', function(){
         }
         let responseMethod = catalog.updateProductById(productId,userId, objWithCategoryAndName)
         expect(responseMethod)
-            .to.be.equal(true)
+            .toEqual(true)
         done()
     })
     it('after category and name are changed has product name is changed', function(done){
         let productChanged = catalog.getProductById(productId);
         let nameChanged = productChanged.name
         expect(nameChanged)
-            .to.be.equal(name)
+            .toEqual(name)
         done()
     })
     it('after category and name are changed has product category is changed', function(done){
         let productChanged = catalog.getProductById(productId);
         let categoryChanged = productChanged.category;
         expect(categoryChanged)
-            .to.be.equal(category)
+            .toEqual(category)
         done()
     })
     
