d1be945cef4e66af5abf96323f61e482b4e35763
wenderu00 <mwmcjr@gmail.com>
2023-11-19 15:28:50 -0300

    switch catalog generateId method

--- tests/catalog/generateId.test.js
+++ tests/catalog/generateId.test.js
@@ -1,19 +1,16 @@
-const chai = require('chai');
 const Catalog = require('../../src/catalog');
-const expect = chai.expect;
 const catalog = new Catalog();
-
 
 describe('generateId catalog class method test', function(){
     it('catlog have generateId method',function(done){
       expect(catalog)
-        .to.have.property('generateId')
+        .toHaveProperty('generateId')
       done()
     })
     
     it('catalog generate id metodo returns length+1', function(done){
       expect(catalog.generateId())
-        .to.be.equal(catalog.products.length+1)
+        .toEqual(catalog.products.length+1)
       done()
     })
 })
