e56c911b6ca07bd4b0c36562bb51b138fa0f01f3
wenderu00 <mwmcjr@gmail.com>
2023-11-19 16:10:55 -0300

    switch catalog addOnCatalog tests

--- tests/catalog/addOnCatalog.test.js
+++ tests/catalog/addOnCatalog.test.js
@@ -1,6 +1,4 @@
-const chai = require('chai');
 const Catalog = require('../../src/catalog');
-const expect = chai.expect;
 const catalog = new Catalog();
 const validObject = {
     name: "monkey",
@@ -12,26 +10,26 @@ const invalidObject = {}
 describe('addOnCatalog catalog class method tests', function(){
     it('catalog have addOnCatalog method', function(done){
         expect(catalog)
-          .to.have.property('addOnCatalog');
+          .toHaveProperty('addOnCatalog');
         done();
       })
       
       it('add one valid object on catalog and catch id', function(done){
         let productId = catalog.addOnCatalog(validObject,userId);
         expect(productId)
-          .to.equals(1)
+          .toEqual(1)
         done()
       })
       
       it('products length to be one', function(done){
         expect(catalog.products.length)
-          .to.equal(1)
+          .toEqual(1)
         done()
       })
       
       it('cant add one invalid object on catalog', function(done){
         expect(catalog.addOnCatalog(invalidObject))
-          .to.equal(false)
+          .toEqual(false)
         done()
       })
 })
